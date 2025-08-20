56fe2b613f30e559417184b2cdca8b515cc4cb9b
Juani <juanireyna2275@gmail.com>
2025-08-20 00:55:34 -0300

    Migracion a postgreSQL

--- backend-api/db.js
+++ backend-api/db.js
@@ -1,16 +1,15 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
-//incializacion de sequelize
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "artishop",  
-  process.env.DB_USER || "postgres",   
-  process.env.DB_PASS || "moncho1720",
-    
-    {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres",
-    logging: false,
+// Inicialización de Sequelize usando DATABASE_URL
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Necesario para Render
+    }
+  }
 });
 
 export default sequelize;
